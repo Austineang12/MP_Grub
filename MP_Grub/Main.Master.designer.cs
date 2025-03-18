619393d3607e29151ecca016c81347d0a03f57a8
shwagg <jcbasa27@gmail.com>
2025-03-18 19:14:41 +0800

    Added Voucher.aspx, Modified Master (added Voucher Hyperlink), and Populated GrubDB(User Table)

--- MP_Grub/Main.Master.designer.cs
+++ MP_Grub/Main.Master.designer.cs
@@ -96,6 +96,15 @@ namespace MP_Grub
         protected global::System.Web.UI.WebControls.HyperLink HyperLinkOrder;
 
         /// <summary>
+        /// HyperLinkVoucher control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLinkVoucher;
+
+        /// <summary>
         /// HyperLinkSupport control.
         /// </summary>
         /// <remarks>
---
+++ MP_Grub/Voucher.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MP_Grub
+{
+	public partial class Voucher : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+        protected void GenerateVoucher(object sender, EventArgs e)
+        {
+            
+            string[] vouchers = { "~/images/Voucher5.png", "~/images/Voucher10.png", "~/images/Voucher20.png" };
+
+            Random rnd = new Random();
+            int index = rnd.Next(vouchers.Length); //to randomize voucher options
+
+            VoucherImage.ImageUrl = vouchers[index];
+        }
+    }
+}
