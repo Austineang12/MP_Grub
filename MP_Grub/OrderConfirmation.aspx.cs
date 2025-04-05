4a380c43b96341887c335557575cf8e6fe8b6807
Alyzza Ocoma <alyzza.ocoma@gmail.com>
2025-04-05 18:21:17 +0800

    Deleted Thank You page.

--- MP_Grub/OrderConfirmation.aspx.cs
+++ MP_Grub/OrderConfirmation.aspx.cs
@@ -13,16 +13,20 @@ namespace MP_Grub
         private static string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\GrubDB.accdb;";
         protected void Page_Load(object sender, EventArgs e)
         {
+            //CATCH FROM FUNCTION SAVEDATA()
             if (Request.HttpMethod == "POST")
             {
                 string action = Request.Form["action"];
 
                 if (action == "submitFeedback")
                 {
-                    // Get rating and feedback time from the form
+                    //GET RATING AND REVIEW DATA
                     string rating = Request.Form["rating"];
                     string feedbackTimeString = Request.Form["feedbackTime"];
                     string feedbackTime = DateTime.TryParse(feedbackTimeString, out DateTime parsedTime) ? parsedTime.ToString() : string.Empty;
+                    string reviewText = Request.Form["reviewText"];
+
+
 
                     if (!string.IsNullOrEmpty(rating) && !string.IsNullOrEmpty(feedbackTime))
                     {
@@ -35,7 +39,7 @@ namespace MP_Grub
                         int userId = Convert.ToInt32(Session["UserID"]);
                         int transactionId = Convert.ToInt32(Session["TransactionID"]);
 
-                        SaveFeedback(userId, transactionId, rating, feedbackTime);
+                        SaveFeedback(userId, transactionId, rating, feedbackTime, reviewText);
                     }
                 }
             }
@@ -43,24 +47,24 @@ namespace MP_Grub
 
         protected void btnOrderDetails_Click(object sender, EventArgs e)
         {
-            Response.Redirect("Order.aspx"); // go back to Order page after submitting info to database
+            //SKIPS THE RATING AND REVIEW AND GOES BACK TO ORDER PAGE
+            Response.Redirect("Order.aspx"); 
         }
 
         protected void btnDone_Click(object sender, EventArgs e)
         {
-            Response.Redirect("ThankYou.aspx"); //to leave review
+            //AFTER SAVES THE RATING AND REVIEW TO DATABASE
+            Response.Redirect("Order.aspx");
         }
 
-        private void SaveFeedback(int userID, int transactionID, string rating, string feedbackTime)
+        private void SaveFeedback(int userID, int transactionID, string rating, string feedbackTime, string comment)
         {
             using (OleDbConnection conn = new OleDbConnection(connectionString))
             {
                 conn.Open();
-
-                // Step 1: Insert feedback data
                 string insertQuery = @"
-            INSERT INTO Feedback (User_ID, Transaction_ID, Rating, Feedback_Time)
-            VALUES (?, ?, ?, ?)";
+                    INSERT INTO Feedback (User_ID, Transaction_ID, Rating, Feedback_Time, Comment)
+                    VALUES (?, ?, ?, ?, ?)";
 
                 using (OleDbCommand cmd = new OleDbCommand(insertQuery, conn))
                 {
@@ -68,17 +72,10 @@ namespace MP_Grub
                     cmd.Parameters.AddWithValue("?", transactionID);
                     cmd.Parameters.AddWithValue("?", rating);
                     cmd.Parameters.AddWithValue("?", feedbackTime);
+                    cmd.Parameters.AddWithValue("?", comment);
+
 
                     cmd.ExecuteNonQuery(); 
-                }
-
-                string selectQuery = "SELECT @@IDENTITY"; //last inserted identity value
-
-                using (OleDbCommand cmd = new OleDbCommand(selectQuery, conn))
-                {
-                    var feedbackId = cmd.ExecuteScalar(); 
-                    // Storing the feedback ID in the session
-                    Session["FeedbackID"] = feedbackId;
                 }
             }
         }
